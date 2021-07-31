1c656d432a68b32fdace3247f29fe2db2d899675
FBach6i <78297442+FBach6i@users.noreply.github.com>
2021-07-31 20:18:47 +0200

    introduce SuperSampler

---
+++ src/fbach6i/raytracing/rendering/sampler/SuperSampler.java
@@ -0,0 +1,29 @@
+package fbach6i.raytracing.rendering.sampler;
+
+import java.util.ArrayList;
+
+import carlvbn.raytracing.pixeldata.Color;
+import carlvbn.raytracing.pixeldata.PixelData;
+import carlvbn.raytracing.rendering.Renderer;
+import carlvbn.raytracing.rendering.Scene;
+import fbach6i.raytracing.rendering.sampler.math.Quadrant;
+import fbach6i.raytracing.rendering.sampler.math.Vector2;
+
+public class SuperSampler extends MultiRaySampler {
+
+    @Override
+    public PixelData samplePixel(Scene scene, float u, float v) {
+        Quadrant pixel = new Quadrant(new Vector2(u,v), _pixelBlockSize);
+        ArrayList<Vector2> samplePoints = pixel.getSamplePoints(3);
+        ArrayList<Color> rayColors = new ArrayList<Color>();
+
+
+        for (Vector2 samplePoint: samplePoints) { 
+            PixelData pixelData = Renderer.computePixelInfo(scene,samplePoint.getXCoordinate(), samplePoint.getYCoordinate());
+            rayColors.add(pixelData.getColor());
+        }
+
+        return new PixelData(Color.average(rayColors),0,0);
+    }
+    
+}
